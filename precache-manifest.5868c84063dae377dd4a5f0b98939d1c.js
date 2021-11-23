71e2c1a8521e5fdc08ea1d7b62fb5ff6a024aef1
laneyching <lching@wustl.edu>
2021-11-22 23:19:56 -0600

    Updates

---
+++ precache-manifest.5868c84063dae377dd4a5f0b98939d1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c33815ab79dd4a249ad",
+    "url": "/09-todo-react-lilching/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "0c33815ab79dd4a249ad",
+    "url": "/09-todo-react-lilching/static/js/main.0c33815a.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-lilching/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "dbe9cedc02d39c86715f",
+    "url": "/09-todo-react-lilching/static/js/runtime~main.dbe9cedc.js"
+  },
+  {
+    "revision": "de199ee640c85e94b1a586105994cbf3",
+    "url": "/09-todo-react-lilching/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-react/precache-manifest.857ce18bebcba928b3be6d240402ba41.js"
+  "/09-todo-react-lilching/precache-manifest.5868c84063dae377dd4a5f0b98939d1c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/todo-react/index.html", {
+workbox.routing.registerNavigationRoute("/09-todo-react-lilching/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
